a91e1a4d84b8e35c4e4d4c35e4eed59aec201f51
shreydd <47713047+shreydd@users.noreply.github.com>
2021-07-13 06:39:45 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import "./styles.css";
+
+var showDB = [
+  "How I Met Your Mother",
+  "Friends",
+  "Mr. Robot",
+  "Silicon Valley",
+  "Brooklyn Nine-Nine",
+  "The Arrow",
+  "Titans"
+];
+
+var showRating = [
+  "9/10",
+  "9.5/10",
+  "9.2/10",
+  "8/10",
+  "9.3/10",
+  "9.4/10",
+  "8/10"
+];
+
+var musicDB = ["One love", "Oh, death", "Thrill of it", "Masakali"];
+var musicArtist = ["Blue", "Steven Suptic", "Conro", "Mohit Chauhan"];
+
+export default function App() {
+  var [toggle, setToggle] = useState("");
+
+  const musicList = () => {
+    toggle = musicDB.map((item, value) => (
+      <div key={value} className="listPlace">
+        <h3>{item} </h3> <p> {musicArtist[value]} </p>
+      </div>
+    ));
+    setToggle(toggle);
+  };
+
+  const showList = () => {
+    toggle = showDB.map((item, value) => (
+      <div key={value} className="listPlace">
+        <h3> {item} </h3> <p> {showRating[value]} </p>
+      </div>
+    ));
+    setToggle(toggle);
+  };
+
+  // const toggleList = () => {
+  //   setToggle(true);
+  // };
+
+  return (
+    <div className="App">
+      <h2 className="Heading">
+        {" "}
+        <span role="img" aria-label="idea emoji">
+          {" "}
+          💡
+        </span>{" "}
+        Recommendations
+      </h2>
+      <small> Some of my recommendations </small>
+      <div>
+        <button onClick={musicList}> Music </button>
+        <button onClick={showList}> TV Shows </button>
+      </div>
+      <hr></hr>
+      {toggle}
+    </div>
+  );
+}
